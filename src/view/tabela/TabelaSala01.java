25b6f94525d60d3b917ebf68b79ecca14d0f649d
lucas queiroz <lukasqueiroz54gmail.com>
2019-06-12 17:34:09 -0300

    atualizando

--- src/view/tabela/TabelaSala01.java
+++ src/view/tabela/TabelaSala01.java
@@ -3,8 +3,10 @@ package view.tabela;
 import java.awt.BorderLayout;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,128 +20,145 @@ import view.Cadastro;
 public class TabelaSala01 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-    private JPanel painel = null;
-    private JButton btnCadastrarHorario = null;
-    private JScrollPane scrlTabela = null;
-    private JTable tblTabela = null;
-    String salaRecebida;
-  
-    public TabelaSala01(String sala) {
-    	super();
-    	salaRecebida = sala;
-        initialize();
-    }
-    /**
-     * Este metodo inicia a janela
-     * @return void
-     */
-    private void initialize() {
-        this.setSize(900, 600);
-        this.setResizable(false);
-        this.setContentPane(getJContentPane());
-        this.setTitle("Listagem "+salaRecebida);
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        lerDoCVS();
-    }
-    /**
-     * Este metodo inicia o painel
-     * @return painel
-     */
-    private JPanel getJContentPane() {
-        if (painel == null) {
-            painel = new JPanel();
-            painel.setLayout(new BorderLayout());
-            painel.add(getBtnLerDoCvs(), BorderLayout.SOUTH);
-            painel.add(getScrlTabela(), BorderLayout.CENTER);
-        }
-        return painel;
-    }
+	private JPanel painel = null;
+	private JButton btnCadastrarHorario = null;
+	private JScrollPane scrlTabela = null;
+	private JTable tblTabela = null;
+	String salaRecebida;
 
-    /**
-     * Este metodo inicia o botao
-     * @return JButton
-     */
-    private JButton getBtnLerDoCvs() {
-        if (btnCadastrarHorario == null) {
-            btnCadastrarHorario = new JButton();
-            btnCadastrarHorario.setText("Cadastrar Horario");
-            btnCadastrarHorario.addActionListener(new java.awt.event.ActionListener() {
-                public void actionPerformed(java.awt.event.ActionEvent e) {
-                    dispose();
-                	Cadastro cadastro =new Cadastro(salaRecebida);
-                    cadastro.setVisible(true);
-                }
-            });
-        }
-        return btnCadastrarHorario;
-    }
+	public TabelaSala01(String sala) {
+		super();
+		salaRecebida = sala;
+		initialize();
+	}
 
-    /**
-     * Este metodo ler do arquivo selecionado
-     */
-    protected void lerDoCVS() {
-        try {
-            BufferedReader reader = null;
-            try {
-            	if (salaRecebida.equals("sala01")) {
-            		reader = new BufferedReader(new FileReader(salaRecebida+".txt"));					
+	/**
+	 * Este metodo inicia a janela
+	 * 
+	 * @return void
+	 */
+	private void initialize() {
+		this.setSize(900, 600);
+		this.setResizable(false);
+		this.setContentPane(getJContentPane());
+		this.setTitle("Listagem " + salaRecebida);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		lerDoCVS();
+	}
+
+	/**
+	 * Este metodo inicia o painel
+	 * 
+	 * @return painel
+	 */
+	private JPanel getJContentPane() {
+		if (painel == null) {
+			painel = new JPanel();
+			painel.setLayout(new BorderLayout());
+			painel.add(getBtnLerDoCvs(), BorderLayout.SOUTH);
+			painel.add(getScrlTabela(), BorderLayout.CENTER);
+		}
+		return painel;
+	}
+
+	/**
+	 * Este metodo inicia o botao
+	 * 
+	 * @return JButton
+	 */
+	private JButton getBtnLerDoCvs() {
+		if (btnCadastrarHorario == null) {
+			btnCadastrarHorario = new JButton();
+			btnCadastrarHorario.setText("Cadastrar Horario");
+			btnCadastrarHorario.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					dispose();
+					Cadastro cadastro = new Cadastro(salaRecebida);
+					cadastro.setVisible(true);
 				}
-            	else if(salaRecebida.equals("sala02")) {
-            		reader = new BufferedReader(new FileReader(salaRecebida+".txt"));
-            	}
-            	else if(salaRecebida.equals("sala03")) {
-            		reader = new BufferedReader(new FileReader(salaRecebida+".txt"));
-            	}
-            	else if(salaRecebida.equals("sala04")) {
-            		reader = new BufferedReader(new FileReader(salaRecebida+".txt"));
-            	}
-            	else if(salaRecebida.equals("sala05")) {
-            		reader = new BufferedReader(new FileReader(salaRecebida+".txt"));
-            	}
+			});
+		}
+		return btnCadastrarHorario;
+	}
 
-                //Leitura do cabecalho
-                String linha = reader.readLine();
-                if (linha == null) {
-                	BufferedWriter writer = new BufferedWriter(new FileWriter(salaRecebida+".txt"));
-                	writer.write("Nome,Matricula,Horario de entrada,Horario de Saida,piloto,marcador");
-                }
+	/**
+	 * Este metodo ler do arquivo selecionado
+	 */
+	protected void lerDoCVS() {
+		File file = new File(salaRecebida + ".txt");
 
-                //Criamos nosso model com o cabecalho
-                //O split separa as colunas. Ler a primeira linha 
-                CVSTableModel model = new CVSTableModel(linha.split(","));
+		// Se o arquivo nao existir, ele gera
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// JOptionPane.showInputDialog(null, "nao criou cara");
+				e.printStackTrace();
+			}
+		}
+		try {
+			BufferedReader reader = null;
+			try {
+				if (salaRecebida.equals("sala01")) {
+					reader = new BufferedReader(new FileReader(salaRecebida + ".txt"));
+				} else if (salaRecebida.equals("sala02")) {
+					reader = new BufferedReader(new FileReader(salaRecebida + ".txt"));
+				} else if (salaRecebida.equals("sala03")) {
+					reader = new BufferedReader(new FileReader(salaRecebida + ".txt"));
+				} else if (salaRecebida.equals("sala04")) {
+					reader = new BufferedReader(new FileReader(salaRecebida + ".txt"));
+				} else if (salaRecebida.equals("sala05")) {
+					reader = new BufferedReader(new FileReader(salaRecebida + ".txt"));
+				}
 
-                //Leitura das linhas seguintes
-                linha = reader.readLine();
-                while (linha != null) {
-                    model.adiciciona(linha); // Cada linha lida entra no model
-                    linha = reader.readLine();
-                }
-                //Definimos nosso model na table.
-                getTblTabela().setModel(model);
-            } finally {
-                if (reader != null) {
-                    reader.close();
-                }
-            }
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, e.getMessage());
-        }
-    }
+				// Leitura do cabecalho
+				String linha = reader.readLine();
+				if (linha == null) {
+					FileWriter fw = new FileWriter(file.getAbsoluteFile());
+					BufferedWriter bw = new BufferedWriter(fw);
 
-    private JScrollPane getScrlTabela() {
-        if (scrlTabela == null) {
-            scrlTabela = new JScrollPane();
-            scrlTabela.setViewportView(getTblTabela());
-        }
-        return scrlTabela;
-    }
+					// Escreve e fecha arquivo
+					bw.write("Nome,Matricula,Horario de entrada,Horario de Saida,piloto,marcador");
+					bw.close();
+				}
 
-    private JTable getTblTabela() {
-        if (tblTabela == null) {
-            tblTabela = new JTable();
-        }
-        return tblTabela;
-    }
+				// Criamos nosso model com o cabecalho
+				// O split separa as colunas. Ler a primeira linha
+				CVSTableModel model = new CVSTableModel(linha.split(","));
+
+				// Leitura das linhas seguintes
+				linha = reader.readLine();
+				while (linha != null) {
+					model.adiciciona(linha); // Cada linha lida entra no model
+					linha = reader.readLine();
+				}
+				// Definimos nosso model na table.
+				getTblTabela().setModel(model);
+			} finally {
+				if (reader != null) {
+					reader.close();
+				}
+			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(this, "iniciando o arquivo para guardar todos os horarios");
+		}
+
+	}
+
+	private JScrollPane getScrlTabela() {
+		if (scrlTabela == null) {
+			scrlTabela = new JScrollPane();
+			scrlTabela.setViewportView(getTblTabela());
+		}
+		return scrlTabela;
+	}
+
+	private JTable getTblTabela() {
+		if (tblTabela == null) {
+			tblTabela = new JTable();
+		}
+		return tblTabela;
+	}
 
 //    public static void main(String[] args) {
 //        new TabelaSala01().setVisible(true);
